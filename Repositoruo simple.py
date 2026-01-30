338b44654848b519093fc384d54e6a680fe97348
¨Alexis¨ <alexisconro4@gmail.com>
2026-01-30 13:44:23 -0600

    De verdad no me cansare de decir que es miuy tedioso

--- Repositoruo simple.py
+++ Repositoruo simple.py
@@ -1,38 +1,12 @@
-def sumar(a, b):
-    return a + b
+def doble(numero):
+    return numero * 2
 
-def restar(a, b):
-    return a - b
-
-def multiplicar(a, b):
-    return a * b
-
-def dividir(a, b):
-    if b == 0:
-        return "Error: división por cero"
-    return a / b
+def es_par(numero):
+    return numero % 2 == 0
 
 
 if __name__ == "__main__":
-    print("Calculadora Simple")
-    print("1. Sumar")
-    print("2. Restar")
-    print("3. Multiplicar")
-    print("4. Dividir")
+    n = int(input("Ingresa un número: "))
 
-    opcion = input("Elige una opción (1-4): ")
-
-    a = float(input("Primer número: "))
-    b = float(input("Segundo número: "))
-
-    if opcion == "1":
-        print("Resultado:", sumar(a, b))
-    elif opcion == "2":
-        print("Resultado:", restar(a, b))
-    elif opcion == "3":
-        print("Resultado:", multiplicar(a, b))
-    elif opcion == "4":
-        print("Resultado:", dividir(a, b))
-    else:
-        print("Opción inválida")
-        
+    print("El doble es:", doble(n))
+    print("¿Es par?", es_par(n))
